458424f17d2e07cfbc050f4f11e49fc8aca48387
Kuldyaev <slavalion@mail.ru>
2024-02-12 02:57:04 +0200

    AdressComponent for 1280px

--- src/components/AddressComponent.vue
+++ src/components/AddressComponent.vue
@@ -19,13 +19,19 @@
     <FlexBox class="tallin">
       <svg
         id="svgback"
-        width="611"
-        height="336"
-        viewBox="0 0 611 336"
+        :width="mesurRect[0]"
+        :height="mesurRect[1]"
+        :viewBox="mesurRect[3]"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
       >
-        <rect opacity="0.05" width="611" height="336" rx="24" fill="#F7F7FA" />
+        <rect
+          opacity="0.05"
+          :width="mesurRect[0]"
+          :height="mesurRect[1]"
+          :rx="mesurRect[2]"
+          fill="#F7F7FA"
+        />
       </svg>
       <FlexBox
         class="tallinInfo"
@@ -37,7 +43,7 @@
           <h6 class="white">Таллинский офис</h6>
           <h6>3 korrus, Metro Plaza, Viru väljak 2</h6>
           <a href="tel:+3726104253">
-            <h6>+372 610-42-53{{ xxx }}</h6>
+            <h6>+372 610-42-53</h6>
           </a>
         </FlexBox>
         <FlexBox
@@ -69,12 +75,38 @@
 
 <script setup lang="ts">
 import FlexBox from "./FlexBox.vue";
-import { computed } from "vue";
+import { computed, ComputedRef } from "vue";
+
+type measures = ComputedRef<[number, number, number, string | undefined]>;
 
 const props = defineProps(["screenWidth"]);
 
-const xxx = computed(() => {
-  return props.screenWidth;
+const mesurRect: measures = computed(() => {
+  if (props.screenWidth > 1920) {
+    return [
+      Math.round(0.31823 * props.screenWidth),
+      Math.round(0.175 * props.screenWidth),
+      Math.round(0.0125 * props.screenWidth),
+      "0 0 " +
+        String(Math.round(0.31823 * props.screenWidth)) +
+        " " +
+        String(Math.round(0.175 * props.screenWidth)),
+    ];
+  } else if (1900 <= props.screenWidth && props.screenWidth <= 1920) {
+    return [611, 336, 24, "0 0 611 336"];
+  } else if (1281 <= props.screenWidth && props.screenWidth <= 1899) {
+    return [
+      Math.round(0.32158 * props.screenWidth),
+      Math.round(0.17684 * props.screenWidth),
+      Math.round(0.01263 * props.screenWidth),
+      "0 0 " +
+        String(Math.round(0.32158 * props.screenWidth)) +
+        " " +
+        String(Math.round(0.17684 * props.screenWidth)),
+    ];
+  } else {
+    return [407.33, 228.75, 16, "0 0 407 229"];
+  }
 });
 </script>
 
@@ -175,6 +207,99 @@ a {
     min-height: 0.729vw;
     margin-left: 0.365vw;
     margin-top: 0.26vw;
+  }
+}
+
+@media (min-width: 1281px) and (max-width: 1899px) {
+  .addresses {
+    width: 32.158vw;
+    height: 26.105vw;
+    margin-top: 10.263vw;
+  }
+  .msk,
+  .nn,
+  .tln {
+    height: 5.053vw;
+    width: 10.526vw;
+  }
+  .nn {
+    margin-left: 4.368vw;
+  }
+  .tallin {
+    width: 32.158vw;
+    height: 17.684vw;
+  }
+  .tallinInfo {
+    padding: 1.737vw 1.737vw 1.737vw 2.053vw;
+    height: 14.211vw;
+  }
+  .mainLinkBlock,
+  .linkBlockText {
+    height: 5.316vw;
+  }
+  .osLogo {
+    min-width: 4.737vw;
+    max-width: 4.737vw;
+    min-height: 4.737vw;
+  }
+  .osLink {
+    min-width: 0.737vw;
+    max-width: 0.737vw;
+    min-height: 0.737vw;
+    margin-left: 0.368vw;
+    margin-top: 0.263vw;
+  }
+}
+
+//1280px
+@media (max-width: $middle-screen) {
+  .addresses {
+    width: 407.33px;
+    height: 337.68px;
+    margin-top: 130.04px;
+  }
+  .msk,
+  .nn,
+  .tln {
+    height: 71px;
+    width: 195px;
+  }
+  .msk,
+  .nn,
+  .tln > h6 {
+    font-size: 12px;
+    line-height: 14.4px;
+  }
+  .nn {
+    margin-left: 30px;
+  }
+  .tallin {
+    width: 407.33px;
+    height: 228.75px;
+  }
+  .tallinInfo {
+    padding: 22px 22px 22px 26px;
+    height: 183.1px;
+  }
+  .mainLinkBlock,
+  .linkBlockText {
+    height: 68.08px;
+  }
+  .linkBlockText > h6 {
+    font-size: 12px;
+    line-height: 14.4px;
+  }
+  .osLogo {
+    min-width: 61.27px;
+    max-width: 61.27px;
+    min-height: 61.27px;
+  }
+  .osLink {
+    min-width: 9.53px;
+    max-width: 9.53px;
+    min-height: 9.33px;
+    margin-left: 5px;
+    margin-top: 4px;
   }
 }
 </style>
