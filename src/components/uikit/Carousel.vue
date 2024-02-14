f57680c2528fd9321c776122de7b01c7999c611e
Kuldyaev <slavalion@mail.ru>
2024-02-15 01:49:53 +0200

    Carousel base

--- src/components/uikit/Carousel.vue
+++ src/components/uikit/Carousel.vue
@@ -1,17 +1,31 @@
 <template>
-  <FlexBox class="carousel">
-    <slot />
-  </FlexBox>
+  <ul class="carousel">
+    <li v-for="(slide, index) in carouselSlides" :key="index">
+      <CarouselSlide />
+    </li>
+  </ul>
 </template>
 
 <script setup lang="ts">
-import FlexBox from "./../../components/FlexBox.vue";
+import CarouselSlide from "./CarouselSlide.vue";
+
+const carouselSlides = [0, 1, 2];
 </script>
 
 <style scoped lang="scss">
+ul {
+  padding: 0;
+  margin: 0;
+}
+li {
+  list-style-type: none;
+}
 .carousel {
   height: 507px;
-  width: 100vw;
-  position: relative;
+  position: absolute;
+  z-index: 1;
+  display: flex;
+  flex-direction: row;
+  left: -70vw;
 }
 </style>
