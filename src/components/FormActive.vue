d6f85daa509876c104c337b7eef15e49dfe688b6
Kuldyaev <slavalion@mail.ru>
2024-03-10 22:22:34 +0200

    requestForm Stages

---
+++ src/components/FormActive.vue
@@ -0,0 +1,11 @@
+<template>
+  <FlexBox class="info">
+    <FlexBox>Form</FlexBox>
+  </FlexBox>
+</template>
+
+<script setup lang="ts">
+import FlexBox from "./FlexBox.vue";
+</script>
+
+<style scoped lang="scss"></style>
---
+++ src/components/RequestError.vue
@@ -0,0 +1,11 @@
+<template>
+  <FlexBox class="info">
+    <FlexBox>Error</FlexBox>
+  </FlexBox>
+</template>
+
+<script setup lang="ts">
+import FlexBox from "./FlexBox.vue";
+</script>
+
+<style scoped lang="scss"></style>
