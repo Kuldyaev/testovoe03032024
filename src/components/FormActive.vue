7e8e3503e2d513c43eeae5c650afda8040034a03
Kuldyaev <slavalion@mail.ru>
2024-03-20 12:01:37 +0200

    FORM added

--- src/components/FormActive.vue
+++ src/components/FormActive.vue
@@ -1,17 +1,45 @@
 <template>
-  <FlexBox class="info">
-    <FlexBox>Form</FlexBox>
+  <FlexBox class="info" direction="column">
+    <FlexBox direction="column" justify="start">
+      <h4>Что Вас интересует?</h4>
+      <FlexBox>
+        <Button
+          category="circleRight"
+          @click="console.log('nsdlknfl')"
+          @stopmoving="console.log('nsdlknfl')"
+        />
+        <Button
+          category="circleRight"
+          @click="console.log('nsdlknfl')"
+          @stopmoving="console.log('nsdlknfl')"
+        />
+        <Button
+          category="circleRight"
+          @click="console.log('nsdlknfl')"
+          @stopmoving="console.log('nsdlknfl')"
+        />
+        <Button
+          category="circleRight"
+          @click="console.log('nsdlknfl')"
+          @stopmoving="console.log('nsdlknfl')"
+        />
+        <Button
+          category="circleRight"
+          @click="console.log('nsdlknfl')"
+          @stopmoving="console.log('nsdlknfl')"
+        />
+      </FlexBox>
+    </FlexBox>
+    <FlexBox>
+      <form action="/action_page.php">
+        First name:<br />
+        <input type="text" value="Mickey" /><br />
+        Last name:<br />
+        <input type="text" name="lastname" value="Mouse" /><br /><br />
+        <input type="submit" value="Submit" />
+      </form>
+    </FlexBox>
     tHIS IS ACTIVE FORM
-    <Button
-      category="circleRight"
-      @click="console.log('nsdlknfl')"
-      @stopmoving="console.log('nsdlknfl')"
-    />
-    <Button
-      category="circleRight"
-      @click="console.log('nsdlknfl')"
-      @stopmoving="console.log('nsdlknfl')"
-    />
   </FlexBox>
 </template>
 
